e3c5352ee2ad53151d6dae2427e9076fc21d19e1
himanshu19k <himanshuk@leewayhertz.com>
2021-10-17 14:06:27 +0530

    devnet links

--- xdc-net-props/helpers/get-explorer-links.js
+++ xdc-net-props/helpers/get-explorer-links.js
@@ -1,133 +1,127 @@
-const { networkIDs } = require("./enum");
+const { networkIDs } = require('./enum')
 const {
-  MAINNET_CODE,
-  ROPSTEN_CODE,
-  RINKEBY_CODE,
-  GOERLI_CODE,
-  KOVAN_CODE,
-  SOKOL_CODE,
-  POA_CORE_CODE,
-  XDAI_CODE,
-  XDC_CODE,
-  CLASSIC_CODE,
-  XDC_DEVNET_CODE,
-  XDC_TESTNET_CODE,
-} = networkIDs;
+	MAINNET_CODE,
+	ROPSTEN_CODE,
+	RINKEBY_CODE,
+	GOERLI_CODE,
+	KOVAN_CODE,
+	SOKOL_CODE,
+	POA_CORE_CODE,
+	XDAI_CODE,
+	XDC_CODE,
+	CLASSIC_CODE,
+	XDC_TESTNET_CODE,
+	XDC_DEVNET_CODE,
+} = networkIDs
 
-const xdcLink = (prefix) => `https://explorer.${prefix}.network`;
+const xdcLink = (prefix) => `https://explorer.${prefix}.network`
+
+const devLink = (prefix) =>  `https://${prefix}.apothem.network`
 
 const getExplorerAccountLinkFor = (account, network) => {
-  const prefix = getExplorerPrefix(network);
-  return `${xdcLink(prefix)}/addr/${account}`;
-};
-
-const getExplorerTxLinkFor = (hash, network) => {
-  const prefix = getExplorerPrefix(network);
-  // const chain = getExplorerChain(network)
-  return `${xdcLink(prefix)}/tx/${hash}`;
-};
-
-const getExplorerTokenLinkFor = (tokenAddress, account, network) => {
-  const prefix = getExplorerPrefix(network);
-  // const chain = getExplorerChain(network)
-  return `${xdcLink(prefix)}/token/${tokenAddress}`;
-};
-
-const getDevnetAccountLinkFor = (account, network) => {
-  const prefix = getExplorerPrefix(network);
-  return `https://devnet.apothem.network/`;
-};
-
-const getDevnetTxLinkFor = (hash, network) => {
-  const prefix = getExplorerPrefix(network);
-  // const chain = getExplorerChain(network)
-  return `https://devnet.apothem.network/tx/${hash}`;
-};
-
-const getDevnetTokenLinkFor = (tokenAddress, account, network) => {
-  const prefix = getExplorerPrefix(network);
-  // const chain = getExplorerChain(network)
-  return `https://devnet.apothem.network/token/${tokenAddress}`;
-};
-
-function getExplorerChain(network) {
-  const net = parseInt(network);
-  let chain;
-  switch (net) {
-    case MAINNET_CODE: // main net
-    case ROPSTEN_CODE: // ropsten testnet
-    case RINKEBY_CODE: // rinkeby testnet
-    case KOVAN_CODE: // kovan testnet
-    case GOERLI_CODE: // Goerli testnet
-      chain = "eth";
-      break;
-    case SOKOL_CODE: // POA Sokol testnet
-    case POA_CORE_CODE: // POA Core
-    case XDAI_CODE: // xDai chain
-      chain = "poa";
-      break;
-    case CLASSIC_CODE: // ETC
-      chain = "etc";
-      break;
-    case XDC_CODE: // XDC mainnet
-      chain = "xdc";
-      break;
-    // case XDC_DEVNET_CODE: // XDC mainnet
-    //   chain = "xdc";
-    //   break;
-    default:
-      chain = "";
-  }
-  return chain;
+	const prefix = getExplorerPrefix(network)
+	return `${xdcLink(prefix)}/address/${account}`
 }
 
-function getExplorerPrefix(network) {
-  const net = parseInt(network);
-  let prefix;
-  switch (net) {
-    case MAINNET_CODE: // main net
-    case CLASSIC_CODE: // ETC
-    case XDC_CODE: // XDC mainnet
-      prefix = "xinfin";
-      break;
-    case ROPSTEN_CODE: // ropsten testnet
-      prefix = "ropsten";
-      break;
-    case RINKEBY_CODE: // rinkeby testnet
-      prefix = "rinkeby";
-      break;
-    case KOVAN_CODE: // kovan testnet
-      prefix = "kovan";
-      break;
-    case SOKOL_CODE: // POA Sokol testnet
-      prefix = "sokol";
-      break;
-    case POA_CORE_CODE: // POA Core
-      prefix = "core";
-      break;
-    case XDAI_CODE: // xDai chain
-      prefix = "dai";
-      break;
-    case GOERLI_CODE: // Goerli testnet
-      prefix = "goerli";
-      break;
-    case XDC_TESTNET_CODE:
-      prefix = "apothem";
-      break;
-    case XDC_DEVNET_CODE:
-		  prefix = 'dev';
-    		break
-    default:
-      prefix = "";
-  }
-  return prefix;
+const getDevenetAccountLinkFor = (account, network) => {
+	return `${devLink}/address/${account}`
+}
+const getExplorerTxLinkFor = (hash, network) => {
+	const prefix = getExplorerPrefix(network)
+	// const chain = getExplorerChain(network)
+	return `${xdcLink(prefix)}/tx/${hash}`
+}
+
+
+const getDevnetTxLinkFor = (hash, network) => {
+	return `${devLink}/tx/${hash}`
+}
+
+const getExplorerTokenLinkFor = (tokenAddress, account, network) => {
+	const prefix = getExplorerPrefix(network)
+	// const chain = getExplorerChain(network)
+	return `${xdcLink(prefix)}/tokens/${account}`
+}
+
+const getDevnetTokenLinkFor = (tokenAddress, account, network) => {
+	
+	return `${devLink}/tokens/${account}`
+}
+function getExplorerChain (network) {
+	const net = parseInt(network)
+	let chain
+	switch (net) {
+	case MAINNET_CODE: // main net
+	case ROPSTEN_CODE: // ropsten testnet
+	case RINKEBY_CODE: // rinkeby testnet
+	case KOVAN_CODE: // kovan testnet
+	case GOERLI_CODE: // Goerli testnet
+		chain = 'eth'
+		break
+	case SOKOL_CODE: // POA Sokol testnet
+	case POA_CORE_CODE: // POA Core
+	case XDAI_CODE: // xDai chain
+		chain = 'poa'
+		break
+	case CLASSIC_CODE: // ETC
+		chain = 'etc'
+		break
+	case XDC_CODE: // XDC mainnet
+		chain = 'xdc'
+		break
+	default:
+		chain = ''
+	}
+	return chain
+}
+
+function getExplorerPrefix (network) {
+	const net = parseInt(network)
+	let prefix
+	switch (net) {
+	case MAINNET_CODE: // main net
+	case CLASSIC_CODE: // ETC
+	case XDC_CODE: // XDC mainnet
+		prefix = 'xinfin'
+		break
+	case ROPSTEN_CODE: // ropsten testnet
+		prefix = 'ropsten'
+		break
+	case RINKEBY_CODE: // rinkeby testnet
+		prefix = 'rinkeby'
+		break
+	case KOVAN_CODE: // kovan testnet
+		prefix = 'kovan'
+		break
+	case SOKOL_CODE: // POA Sokol testnet
+		prefix = 'sokol'
+		break
+	case POA_CORE_CODE: // POA Core
+		prefix = 'core'
+		break
+	case XDAI_CODE: // xDai chain
+		prefix = 'dai'
+		break
+	case GOERLI_CODE: // Goerli testnet
+		prefix = 'goerli'
+		break
+	case XDC_TESTNET_CODE:
+		prefix = 'apothem'
+		break
+	case XDC_DEVNET_CODE:
+		prefix = 'devnet'
+		break	
+	default:
+		prefix = ''
+	}
+	return prefix
 }
 
 module.exports = {
-  getExplorerAccountLinkFor,
-  getExplorerTxLinkFor,
-  getExplorerTokenLinkFor,
-  getDevnetAccountLinkFor,
-  getDevnetTxLinkFor,
-  getDevnetTokenLinkFor,
-};
+	getExplorerAccountLinkFor,
+	getExplorerTxLinkFor,
+	getExplorerTokenLinkFor,
+	getDevenetAccountLinkFor,
+	getDevnetTxLinkFor,
+	getDevnetTokenLinkFor,
+}
