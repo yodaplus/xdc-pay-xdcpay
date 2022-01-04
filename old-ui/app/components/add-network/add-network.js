718677a99cabf2cbb08090eb4846792f05b6696c
ayushkul <akcool.ayush.svm@gmail.com>
2022-01-04 12:12:45 +0530

    Add Contact Issue

--- old-ui/app/components/add-network/add-network.js
+++ old-ui/app/components/add-network/add-network.js
@@ -2,83 +2,88 @@ import React from 'react'
 
 const AddNetworkComponent = (props) => {
   // eslint-disable-next-line react/prop-types
-  const { onBackClick, onStateChange, onAddNetworkClicked, warningMsg, viewNetworkObj, state } = props
-  const { networkName, rpcUrl, chainId, currencySymbol, explorerLink } = state
+  const {onBackClick, onStateChange, onAddNetworkClicked, warningMsg, viewNetworkObj, state} = props
+  const {networkName, rpcUrl, chainId, currencySymbol, explorerLink} = state
   const isPermanentNetwork = viewNetworkObj && viewNetworkObj.isPermanent
 
   return (
-    <div className="flex-column flex-grow" style={{ maxHeight: '585px', overflowY: 'auto' }}>
-      <div className="section-title flex-row" style={{ borderBottom: '1px solid #E3E7EB', paddingBottom: '17px' }}>
-        <img src="/images/Assets/BackArrow.svg" style={{ marginLeft: '12px', cursor: 'pointer' }} onClick={onBackClick} />
-        <h2 style={{ marginLeft: '88px', fontFamily: 'Inter-bold' }}>{`${viewNetworkObj ? 'View' : 'Add'} Network`}</h2>
+    <div className="flex-column flex-grow" style={{maxHeight: '585px', overflowY: 'auto'}}>
+      <div className="section-title flex-row" style={{borderBottom: '1px solid #E3E7EB', paddingBottom: '17px'}}>
+        <img src="/images/Assets/BackArrow.svg" style={{marginLeft: '12px', cursor: 'pointer'}} onClick={onBackClick}/>
+        <h2 style={{marginLeft: '88px', fontFamily: 'Inter-bold'}}>{`${viewNetworkObj ? 'View' : 'Add'} Network`}</h2>
       </div>
-      <div style={{ margin: '18px 50px' }}>
-        <label className="word" style={{ fontFamily: 'Inter-Medium' }}>Network Name</label>
-        <br />
-        <div style={{ marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px',width:'265px' }}>
-          <input disabled={isPermanentNetwork} className="input large-input" type="text" name="networkName" value={networkName}
-            onChange={onStateChange} style={{ border: 'none', color: '#2A2A2A' }} />
+      <div style={{margin: '18px 50px'}}>
+        <label className="word" style={{fontFamily: 'Inter-Medium'}}>Network Name</label>
+        <br/>
+        <div style={{marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px', width: '265px'}}>
+          <input disabled={isPermanentNetwork} className="input large-input" type="text" name="networkName"
+                 value={networkName}
+                 onChange={onStateChange} style={{border: 'none', color: '#2A2A2A'}}/>
         </div>
-        <label className="word" style={{ fontFamily: 'Inter-Medium' }}>New RPC URL</label>
-        <br />
-        <div style={{ marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px',width: '265px',   }}>
-          <input disabled={isPermanentNetwork} className="input large-input" id="new_rpc" type="text" name="rpcUrl" value={rpcUrl}
-            onChange={onStateChange} style={{ border: 'none', color: '#2A2A2A' }} />
+        <label className="word" style={{fontFamily: 'Inter-Medium'}}>New RPC URL</label>
+        <br/>
+        <div style={{marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px', width: '265px'}}>
+          <input disabled={isPermanentNetwork} className="input large-input" id="new_rpc" type="text" name="rpcUrl"
+                 value={rpcUrl}
+                 onChange={onStateChange} style={{border: 'none', color: '#2A2A2A'}}/>
         </div>
         {warningMsg && <div className="error">{warningMsg}</div>}
-        <label className="word" style={{ fontFamily: 'Inter-Medium' }}>Chain ID</label>
-        <br />
-        <div style={{ marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px',width: '265px', }}>
-          <input disabled={isPermanentNetwork} className="input large-input" type="text" style={{ border: 'none', color: '#2A2A2A' }}
-            name="chainId" onChange={onStateChange} value={chainId} />
+        <label className="word" style={{fontFamily: 'Inter-Medium'}}>Chain ID</label>
+        <br/>
+        <div style={{marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px', width: '265px'}}>
+          <input disabled={isPermanentNetwork} className="input large-input" type="text"
+                 style={{border: 'none', color: '#2A2A2A'}}
+                 name="chainId" onChange={onStateChange} value={chainId}/>
         </div>
-        <label className="word" style={{ fontFamily: 'Inter-Medium' }}>Currency Symbol (Optional)</label>
-        <br />
-        <div style={{ marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px',width: '265px', }}>
-          <input disabled={isPermanentNetwork} className="input large-input" type="text" style={{ border: 'none', color: '#2A2A2A' }}
-            name="currencySymbol" onChange={onStateChange} value={currencySymbol} />
+        <label className="word" style={{fontFamily: 'Inter-Medium'}}>Currency Symbol (Optional)</label>
+        <br/>
+        <div style={{marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px', width: '265px'}}>
+          <input disabled={isPermanentNetwork} className="input large-input" type="text"
+                 style={{border: 'none', color: '#2A2A2A'}}
+                 name="currencySymbol" onChange={onStateChange} value={currencySymbol}/>
         </div>
-        <label className="word" style={{ fontFamily: 'Inter-Medium' }}>Block Explorer (Optional)</label>
-        <br />
-        <div style={{ marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px',width: '265px', }}>
-          <input disabled={isPermanentNetwork} className="input large-input" type="text" style={{ border: 'none', color: '#2A2A2A' }}
-            name="explorerLink" onChange={onStateChange} value={explorerLink} />
+        <label className="word" style={{fontFamily: 'Inter-Medium'}}>Block Explorer (Optional)</label>
+        <br/>
+        <div style={{marginBottom: '24px', border: '1px solid #C7CDD8', borderRadius: '4px', width: '265px'}}>
+          <input disabled={isPermanentNetwork} className="input large-input" type="text"
+                 style={{border: 'none', color: '#2A2A2A'}}
+                 name="explorerLink" onChange={onStateChange} value={explorerLink}/>
         </div>
         {isPermanentNetwork ? '' :
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{display: 'flex', justifyContent: 'space-between'}}>
             <div className="button" onClick={onBackClick}
-              style={{
-                fontFamily: 'Inter-Medium',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                fontSize: '14px',
-                background: '#E3E7EB',
-                width: '120px',
-                height: '40px',
-                border: 'none',
-                color: '#2a2a2a',
-              }}> Cancel
+                 style={{
+                   fontFamily: 'Inter-Medium',
+                   display: 'flex',
+                   justifyContent: 'center',
+                   alignItems: 'center',
+                   fontSize: '14px',
+                   background: '#E3E7EB',
+                   width: '120px',
+                   height: '40px',
+                   border: 'none',
+                   color: '#2a2a2a',
+                 }}> Cancel
             </div>
 
             <div className="button"
-              style={{
-                fontFamily: 'Inter-Medium',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                fontSize: '14px',
-                background: '#03BE46',
-                width: '120px',
-                height: '40px',
-                border: 'none',
-                marginRight: '-6px'
-                
-              }}
-              onClick={(event) => {
-                event.preventDefault()
-                onAddNetworkClicked(!!viewNetworkObj)
-              }}
+                 style={{
+                   fontFamily: 'Inter-Medium',
+                   display: 'flex',
+                   justifyContent: 'center',
+                   alignItems: 'center',
+                   fontSize: '14px',
+                   background: '#03BE46',
+                   width: '120px',
+                   height: '40px',
+                   border: 'none',
+                   marginRight: '-6px',
+
+                 }}
+                 onClick={(event) => {
+                   event.preventDefault()
+                   onAddNetworkClicked(!!viewNetworkObj)
+                 }}
             >{`${viewNetworkObj ? 'Update' : 'Add'}`}
             </div>
           </div>}
@@ -86,4 +91,4 @@ const AddNetworkComponent = (props) => {
     </div>
   )
 }
-module.exports = AddNetworkComponent;
+module.exports = AddNetworkComponent
