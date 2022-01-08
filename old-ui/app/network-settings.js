e9df5985ebc040cb6d93daeedd48293cd302b3e8
Kamlesh_Kulkarni <kamlesh@leewayhertz.com>
2022-01-08 10:31:33 +0530

    Connected Site UI

--- old-ui/app/network-settings.js
+++ old-ui/app/network-settings.js
@@ -1,148 +1,61 @@
-// const connect = require('react-redux').connect
-// const actions = require('../../ui/app/actions')
-// const React = require('react')
-
-// class NetworkSettings extends React.Component {
-
-//   onDeleteRPCNetwork = (networkObj) => {
-//     const state = this.props
-//     state.dispatch(actions.delRpcTarget(networkObj))
-//     if (state.metamask.network === networkObj.chainId) {
-//       state.dispatch(actions.setProviderType('xdc'))
-//     }
-//   }
-
-//   render () {
-//     const state = this.props
-//     const networkList = state.metamask.networkList
-//     const frequentRPCList = state.metamask.frequentRpcList
-//     const netList = [...networkList, ...frequentRPCList]
-//     return (
-//       <div className="flex-column flex-grow" style={{
-//         maxHeight: '585px',
-//         overflowY: 'auto',
-//       }}>
-//         <div className="section-title flex-row" style={{borderBottom: '1px solid #E3E7EB', paddingBottom: '17px'}}>
-//           <img src="/images/Assets/BackArrow.svg" style={{marginLeft: '12px', cursor: 'pointer'}} onClick={() => {
-//             state.dispatch(actions.goConfig())
-//           }}/>
-//           <h2 style={{marginLeft: '80px', fontFamily: 'Inter-bold'}}>Network Settings</h2>
-//           <img src="/images/Assets/Add.svg" style={{cursor: 'pointer', position: 'absolute', right: '21px'}}
-//                onClick={() => {
-//                  state.dispatch(actions.showAddNetworkPage())
-//                }}/>
-//         </div>
-//         {netList.map((networkObj) =>
-
-//           <div style={{
-//             padding: ' 11px 17px 11px 13px ',
-//             borderBottom: '1px solid #E3E7EB',
-//             fontFamily: 'inter-medium',
-//             fontSize: '14px',
-//           }} key={networkObj.chainId}>  <svg height="16px" width="26px" position="absolute" left="17px">
-//               <circle style={{ cx: '10' ,cy:"10", r:"6" , fill:networkObj.colorCode ? networkObj.colorCode : '#E58A0F' }} key={networkObj.isPermanent}/>
-//        </svg>  {networkObj.name}
-//             <img src={networkObj.isPermanent ? '/images/Assets/Lock.png' : '/images/Assets/Delete.svg'}
-//                  style={{position: 'absolute', right: '30px', cursor: networkObj.isPermanent ? 'normal' : 'pointer'}}
-//                  onClick={() => !networkObj.isPermanent && this.onDeleteRPCNetwork(networkObj)}/>
-//             <img src="/images/Assets/Arrow.svg" onClick={() => state.dispatch(actions.viewNetwork(networkObj))}
-//               style={{ position: 'absolute', right: '15px', marginTop: '6px', cursor: 'pointer' }} />
-
-//           </div>)
-//         }
-//       </div>
-//     )
-//   }
-// }
-
-// function mapStateToProps (state) {
-//   return {
-//     metamask: state.metamask,
-//     warning: state.appState.warning,
-//   }
-// }
-
-// module.exports = connect(mapStateToProps)(NetworkSettings)
-
-
-
 const connect = require('react-redux').connect
 const actions = require('../../ui/app/actions')
 const React = require('react')
 
 class NetworkSettings extends React.Component {
 
-
-
-  render() {
+  onDeleteRPCNetwork = (networkObj) => {
     const state = this.props
+    state.dispatch(actions.delRpcTarget(networkObj))
+    if (state.metamask.network === networkObj.chainId) {
+      state.dispatch(actions.setProviderType('xdc'))
+    }
+  }
 
+  render () {
+    const state = this.props
+    const networkList = state.metamask.networkList
+    const frequentRPCList = state.metamask.frequentRpcList
+    const netList = [...networkList, ...frequentRPCList]
     return (
       <div className="flex-column flex-grow" style={{
         maxHeight: '585px',
         overflowY: 'auto',
       }}>
-        <div className="section-title flex-row" style={{ borderBottom: '1px solid #E3E7EB', paddingBottom: '17px' }}>
-          <img src="/images/Assets/BackArrow.svg" style={{ marginLeft: '12px', cursor: 'pointer' }} onClick={() => {
+        <div className="section-title flex-row" style={{borderBottom: '1px solid #E3E7EB', paddingBottom: '17px'}}>
+          <img src="/images/Assets/BackArrow.svg" style={{marginLeft: '12px', cursor: 'pointer'}} onClick={() => {
             state.dispatch(actions.goConfig())
-          }} />
-          <h2 style={{ marginLeft: '80px', fontFamily: 'Inter-bold' }}>Connected Sites</h2>
+          }}/>
+          <h2 style={{marginLeft: '80px', fontFamily: 'Inter-bold'}}>Network Settings</h2>
+          <img src="/images/Assets/Add.svg" style={{cursor: 'pointer', position: 'absolute', right: '21px'}}
+               onClick={() => {
+                 state.dispatch(actions.showAddNetworkPage())
+               }}/>
         </div>
+        {netList.map((networkObj) =>
 
-        <div style={{
-          padding: ' 11px 17px 11px 13px ',
-          borderBottom: '1px solid #E3E7EB',
-          fontFamily: 'inter-medium',
-          fontSize: '14px',
-        }}  >
-          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '158', cursor: 'pointer' }} />
+          <div style={{
+            padding: ' 11px 17px 11px 13px ',
+            borderBottom: '1px solid #E3E7EB',
+            fontFamily: 'inter-medium',
+            fontSize: '14px',
+          }} key={networkObj.chainId}>  <svg height="16px" width="26px" position="absolute" left="17px">
+              <circle style={{ cx: '10' ,cy:"10", r:"6" , fill:networkObj.colorCode ? networkObj.colorCode : '#E58A0F' }} key={networkObj.isPermanent}/>
+       </svg>  {networkObj.name}
+            <img src={networkObj.isPermanent ? '/images/Assets/Lock.png' : '/images/Assets/Delete.svg'}
+                 style={{position: 'absolute', right: '30px', cursor: networkObj.isPermanent ? 'normal' : 'pointer'}}
+                 onClick={() => !networkObj.isPermanent && this.onDeleteRPCNetwork(networkObj)}/>
+            <img src="/images/Assets/Arrow.svg" onClick={() => state.dispatch(actions.viewNetwork(networkObj))}
+              style={{ position: 'absolute', right: '15px', marginTop: '6px', cursor: 'pointer' }} />
 
-          <div style={{marginLeft: '40px' }} ><h1>XDCVoting.com</h1></div>
-          
-          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '158', cursor: 'pointer' }} />
-        </div>
-
-
-        <div style={{
-          padding: ' 11px 17px 11px 13px ',
-          borderBottom: '1px solid #E3E7EB',
-          fontFamily: 'inter-medium',
-          fontSize: '14px',
-        }}  >
-          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '204', cursor: 'pointer' }} />
-
-          <div style={{marginLeft: '40px' }} ><h1>opensea.io</h1></div>
-          
-          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '204', cursor: 'pointer' }} />
-        </div>
-
-        <div style={{
-          padding: ' 11px 17px 11px 13px ',
-          borderBottom: '1px solid #E3E7EB',
-          fontFamily: 'inter-medium',
-          fontSize: '14px',
-        }}  >
-          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '112', cursor: 'pointer' }} />
-
-          <div style={{marginLeft: '40px' }} ><h1>app.uniswap.com</h1></div>
-          
-          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '112', cursor: 'pointer' }} />
-        </div>
-
-
+          </div>)
+        }
       </div>
-
-
-
-
-
-
-
     )
   }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps (state) {
   return {
     metamask: state.metamask,
     warning: state.appState.warning,
@@ -150,3 +63,94 @@ function mapStateToProps(state) {
 }
 
 module.exports = connect(mapStateToProps)(NetworkSettings)
+
+
+
+
+
+// Connected site code................... 
+
+// const connect = require('react-redux').connect
+// const actions = require('../../ui/app/actions')
+// const React = require('react')
+
+// class NetworkSettings extends React.Component {
+
+
+
+//   render() {
+//     const state = this.props
+
+//     return (
+//       <div className="flex-column flex-grow" style={{
+//         maxHeight: '585px',
+//         overflowY: 'auto',
+//       }}>
+//         <div className="section-title flex-row" style={{ borderBottom: '1px solid #E3E7EB', paddingBottom: '17px' }}>
+//           <img src="/images/Assets/BackArrow.svg" style={{ marginLeft: '12px', cursor: 'pointer' }} onClick={() => {
+//             state.dispatch(actions.goConfig())
+//           }} />
+//           <h2 style={{ marginLeft: '80px', fontFamily: 'Inter-bold' }}>Connected Sites</h2>
+//         </div>
+
+//         <div style={{
+//           padding: ' 11px 17px 11px 13px ',
+//           borderBottom: '1px solid #E3E7EB',
+//           fontFamily: 'inter-medium',
+//           fontSize: '14px',
+//         }}  >
+//           <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '158', cursor: 'pointer' }} />
+
+//           <div style={{marginLeft: '40px' }} ><h1>XDCVoting.com</h1></div>
+          
+//           <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '158', cursor: 'pointer' }} />
+//         </div>
+
+
+//         <div style={{
+//           padding: ' 11px 17px 11px 13px ',
+//           borderBottom: '1px solid #E3E7EB',
+//           fontFamily: 'inter-medium',
+//           fontSize: '14px',
+//         }}  >
+//           <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '204', cursor: 'pointer' }} />
+
+//           <div style={{marginLeft: '40px' }} ><h1>opensea.io</h1></div>
+          
+//           <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '204', cursor: 'pointer' }} />
+//         </div>
+
+//         <div style={{
+//           padding: ' 11px 17px 11px 13px ',
+//           borderBottom: '1px solid #E3E7EB',
+//           fontFamily: 'inter-medium',
+//           fontSize: '14px',
+//         }}  >
+//           <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '112', cursor: 'pointer' }} />
+
+//           <div style={{marginLeft: '40px' }} ><h1>app.uniswap.com</h1></div>
+          
+//           <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '112', cursor: 'pointer' }} />
+//         </div>
+
+
+//       </div>
+
+
+
+
+
+
+
+//     )
+//   }
+// }
+
+// function mapStateToProps(state) {
+//   return {
+//     metamask: state.metamask,
+//     warning: state.appState.warning,
+//   }
+// }
+
+// module.exports = connect(mapStateToProps)(NetworkSettings)
