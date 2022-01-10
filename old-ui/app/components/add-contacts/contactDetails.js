ad8b0648b384524f94f392c2094583b9a22693aa
himanshu19k <himanshuk@leewayhertz.com>
2022-01-10 19:03:11 +0530

    Contact details need some fixes in dataList

--- old-ui/app/components/add-contacts/contactDetails.js
+++ old-ui/app/components/add-contacts/contactDetails.js
@@ -1,90 +1,167 @@
-// const React = require("react");
-// const connect = require("react-redux").connect;
-// const actions = require("../../../../ui/app/actions");
-// import Identicon from "../../../../ui/app/components/identicon";
-// import PropTypes from "prop-types";
-// import { name } from "loglevel";
+const React = require("react");
+const connect = require("react-redux").connect;
+const actions = require("../../../../ui/app/actions");
+import Identicon from "../../../../ui/app/components/identicon";
+const CopyButton = require("../copy/copy-button");
+import PropTypes from "prop-types";
+import { name } from "loglevel";
+const TransactionList = require("../transaction-list");
+const { valuesFor } = require("../../util");
+class ContactDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    const viewContactObj = this.props.viewContactObj;
+    this.state = {
+      contactAddress: viewContactObj ? viewContactObj.address : "",
+      contactName: viewContactObj ? viewContactObj.name : "",
+    };
+  }
 
-// class ContactDetails extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     const viewContactObj = this.props.viewContactObj
-//     this.state = {
-//       address: viewContactObj.address,
-//       name: viewContactObj.address,
-//     }
-//   }
+  render() {
+    const state = this.props;
+    // const contactList = state.metamask.addressBook;
+    const viewContactObj = state;
+    const { contactAddress, contactName } = state;
+    console.log(state, "<===>");
+    const contactList = state.metamask.addressBook;
+    const {
+      transactions,
+      unapprovedMsgs,
+      address,
+      network,
+      shapeShiftTxList,
+      conversionRate,
+    } = this.props;
+    // var selected = props.address || Object.keys(state.addressBook)[0]s
+    return (
+      <div
+        className="flex-column flex-grow"
+        style={{ maxHeight: "585px", overflowY: "auto" }}
+      >
+        <div
+          className="section-title flex-row"
+          style={{
+            // borderBottom: "1px solid #E3E7EB",
+            paddingBottom: "17px",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <img
+            src="/images/Assets/BackArrow.svg"
+            style={{ marginLeft: "17px", cursor: "pointer" }}
+            onClick={() => {
+              state.dispatch(actions.Contacts());
+            }}
+          />
+          <h2 style={{ fontFamily: "Inter-bold", marginLeft: "30px" }}>
+            Contact Details
+          </h2>
+          {/* <img
+            src="/images/Assets/Add.svg"
+            style={{ cursor: "pointer", marginRight: "11px" }}
+            onClick={() => {
+              state.dispatch(actions.showAddContactsPage());
+            }}
+                /> */}
+          <div
+            style={{
+              color: "#2149B9",
+              fontSize: "15px",
+              marginRight: "22px",
+              fontFamily: "Inter-Medium",
+              cursor: "pointer",
+            }}
+            onClick={() =>
+              state.dispatch(actions.showAddContactsPage(viewContactObj))
+            }
+          >
+            Edit
+          </div>
+        </div>
+        <div className="list">
+          {contactList.map((contactObj) => (
+            <div>
+              <div style={{ borderBottom: "1px solid #E3E7EB" }}>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <Identicon
+                    overflow="none"
+                    address={contactObj.address}
+                    diameter={69}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      overflow: "inherit",
+                    }}
+                  />
+                </div>
+                <div style={{ display: "grid", justifyContent: "center" }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      fontSize: "14px",
+                      fontWeight: "600",
+                      marginTop: "5px",
+                      color: "#2a2a2a",
+                    }}
+                  >
+                    {contactObj.name}
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      fontSize: "12px",
+                      color: "#848484",
+                      marginTop: "16px",
+                      fontFamily: "Inter-Medium",
+                    }}
+                  >
+                    Wallet contactAddress
+                  </div>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <div>{contactObj.address}</div>
+                    <CopyButton value={contactObj.address} isWhite={true} />
+                  </div>
+                </div>
+              </div>
 
-//   render() {
-//     const state = this.props;
-//     // const contactList = state.metamask.addressBook;
-//     const viewContactObj = this.props
-  
-    
+              <div>
+                <TransactionList
+                  transactions={transactions.sort((a, b) => b.time - a.time)}
+                  network={network}
+                  unapprovedMsgs={unapprovedMsgs}
+                  conversionRate={conversionRate}
+                  address={contactObj.address}
+                  shapeShiftTxList={shapeShiftTxList}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
-//     return (
-//       <div
-//         className="flex-column flex-grow"
-//         style={{ maxHeight: "585px", overflowY: "auto" }}
-//       >
-//         <div
-//           className="section-title flex-row"
-//           style={{
-//             // borderBottom: "1px solid #E3E7EB",
-//             paddingBottom: "17px",
-//             display: "flex",
-//             justifyContent: "space-between",
-//           }}
-//         >
-//           <img
-//             src="/images/Assets/BackArrow.svg"
-//             style={{ marginLeft: "17px", cursor: "pointer" }}
-//             onClick={() => {
-//                 state.dispatch(actions.Contacts())
-//             }}
-//           />
-//           <h2 style={{ fontFamily: "Inter-bold" }}>Contact Details</h2>
-//           {/* <img
-//             src="/images/Assets/Add.svg"
-//             style={{ cursor: "pointer", marginRight: "11px" }}
-//             onClick={() => {
-//               state.dispatch(actions.showAddContactsPage());
-//             }}
-//                 /> */}
-//                 <div>Edit</div>
-//         </div>
-//         <div className="list">
-         
-          
-            
-//               <div style={{ padding: "9px 0 0 20px",  borderBottom: "1px solid #E3E7EB",}}>
-//                 <Identicon
-//                   overflow="none"
-//                   address={viewContactObj.address}
-//                   diameter={69}
-//                   style={{ display:'flex',alignItems:'center',justifyContent:'center', overflow: "inherit" }}
-//                 />
-//                   {viewContactObj.name}
-//                   <div>
-//                       {'Wallet Address'}
-//                     <div> {viewContactObj.address}</div>
-//                 </div>
-                  
-//               </div>
-           
-//           )
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+module.exports = connect(mapStateToProps)(ContactDetails);
 
-// module.exports = connect(mapStateToProps)(ContactDetails);
-
-// function mapStateToProps(state) {
-//   return {
-//     metamask: state.metamask,
-//     warning: state.appState.warning,
-//     viewContactObj: state.appState.currentviewContactObj,
-//   };
-// }
+function mapStateToProps(state) {
+  return {
+    metamask: state.metamask,
+    warning: state.appState.warning,
+    viewContactObj: state.appState.currentviewContactObj,
+    network: state.metamask.network,
+    networkList: [
+      ...state.metamask.networkList,
+      ...state.metamask.frequentRpcList,
+    ],
+    unapprovedMsgs: valuesFor(state.metamask.unapprovedMsgs),
+    shapeShiftTxList: state.metamask.shapeShiftTxList,
+    transactions: state.metamask.selectedAddressTxList || [],
+    conversionRate: state.metamask.conversionRate,
+    currentCurrency: state.metamask.currentCurrency,
+  };
+}
