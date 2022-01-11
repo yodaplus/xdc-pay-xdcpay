6f1747e7b87ac131745d33d559e87e4ac1483ad0
ayushkul <akcool.ayush.svm@gmail.com>
2022-01-11 13:02:18 +0530

    Sorting Issue, Contact Details screen issues

--- ui/app/actions.js
+++ ui/app/actions.js
@@ -2065,10 +2065,9 @@ function showAddNetworkPage () {
   }
 }
 
-function showAddContactsPage (contactObj) {
+function showAddContactsPage () {
   return {
     type: actions.SHOW_ADDCONTACTS_PAGE,
-    value: contactObj,
   }
 }
 
