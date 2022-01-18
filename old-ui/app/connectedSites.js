5c112efff0d5adff152bce97845c1c11c767cac5
himanshu19k <himanshuk@leewayhertz.com>
2022-01-18 13:24:31 +0530

    connectedSites

--- old-ui/app/connectedSites.js
+++ old-ui/app/connectedSites.js
@@ -16,13 +16,50 @@ class ConnectedSites extends React.Component {
       }}>
         <div className="section-title flex-row" style={{borderBottom: '1px solid #E3E7EB', paddingBottom: '17px'}}>
           <img src="/images/Assets/BackArrow.svg" style={{marginLeft: '12px', cursor: 'pointer'}} onClick={() => {
-            state.dispatch(actions.goConfig())
+            state.dispatch(actions.goHome())
           }}/>
           <h2 style={{marginLeft: '80px', fontFamily: 'Inter-bold'}}>Connected Sites</h2>
-         
-        
            
-          </div>)
+          </div>
+
+          <div style={{
+          padding: ' 11px 17px 11px 13px ',
+          borderBottom: '1px solid #E3E7EB',
+          fontFamily: 'inter-medium',
+          fontSize: '14px',
+        }}  >
+          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '112px', cursor: 'pointer' }} />
+
+          <div style={{marginLeft: '40px' }} ><h1>XDCVoting.com</h1></div>
+          
+          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '112px', cursor: 'pointer' }} />
+        </div>
+
+        <div style={{
+          padding: ' 11px 17px 11px 13px ',
+          borderBottom: '1px solid #E3E7EB',
+          fontFamily: 'inter-medium',
+          fontSize: '14px',
+        }}  >
+          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '158px', cursor: 'pointer' }} />
+
+          <div style={{marginLeft: '40px' }} ><h1>opensea.io</h1></div>
+          
+          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '158px', cursor: 'pointer' }} />
+        </div>
+
+        <div style={{
+          padding: ' 11px 17px 11px 13px ',
+          borderBottom: '1px solid #E3E7EB',
+          fontFamily: 'inter-medium',
+          fontSize: '14px',
+        }}  >
+          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', left: '18px', top: '204px', cursor: 'pointer' }} />
+
+          <div style={{marginLeft: '40px' }} ><h1>app.uniswap.com</h1></div>
+          
+          <img src={'/images/Assets/Delete.svg'} style={{ position: 'absolute', right: '30px', top: '204px', cursor: 'pointer' }} />
+        </div>
         
       </div>
     )
