5c969c656439a48b13463c50ebef5b1dce95bb60
yashmohan1999 <yash@leewayhertz.com>
2022-02-21 19:59:05 +0530

    bug fixes

--- app/scripts/controllers/network/enums.js
+++ app/scripts/controllers/network/enums.js
@@ -60,7 +60,8 @@ const XDC_TESTNET_RPC_ENDPOINT = 'https://apothemxdcpayrpc.blocksscan.io/'
 const XDC_DEVNET_RPC_ENDPOINT = 'https://devnetrpc.apothem.network'
 
 /* Block Explorer URLs */
-const XDC_BLOCK_EXPLORER_URL = 'https://explorer.xdc.network'
+const XDC_BLOCK_EXPLORER_URL = 'https://observer.xdc.org'
+// const XDC_BLOCK_EXPLORER_URL = 'https://explorer.xdc.network'
 const XDC_TESTNET_BLOCK_EXPLORER_URL = 'https://explorer.apothem.network'
 const XDC_DEVNET_BLOCK_EXPLORER_URL = 'https://explorer.devnet.network'
 
